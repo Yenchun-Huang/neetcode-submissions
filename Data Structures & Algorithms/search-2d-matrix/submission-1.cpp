edb5e1623e07b8659f634f392f818f774654dd71
James Huang <70682399+Yenchun-Huang@users.noreply.github.com>
2026-06-17 16:29:54 +0800

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size(), col = matrix[0].size();
+        int r = 0, c = col - 1;
+
+        while (r < row && c >= 0){
+            if (matrix[r][c] > target){
+                c--;
+            }else if (matrix[r][c] < target){
+                r++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
